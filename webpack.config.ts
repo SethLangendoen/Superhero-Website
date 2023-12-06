889632449e9a22666346012245fb756d4607b921
SethLangendoen <sethlangendoen1@gmail.com>
2023-12-06 10:45:56 -0500

    proxy link fix

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,8 +1,20 @@
 const path = require('path');
 
 module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
   devServer: {
     compress: true,
     disableHostCheck: true,
   },
 };
+
